d1d4a5c7f16d129e0d78d8bb08fc9a41bbca51b2
KrishnaSatpute23 <24satputek@rbunagpur.in>
2025-04-18 20:52:02 +0530

    Test.java

---
+++ Test.java
@@ -0,0 +1,24 @@
+class Test {
+    int i;
+    Test t;
+    boolean b;
+    byte bt;
+    float ft;
+}
+    Public class Main {
+    Public
+
+    static void main(String args[]) {
+
+        Test t = new Test();
+        System.out.println(t.i);
+
+        System.out.println(t.t);
+
+        System.out.println(t.b);
+
+        System.out.println(t.bt);
+
+        System.out.println(t.ft);
+    }
+}
