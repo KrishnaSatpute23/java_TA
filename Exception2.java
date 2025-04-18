b06aeafc5f50be4e7cd00031eacd61ebf6c031a9
KrishnaSatpute23 <24satputek@rbunagpur.in>
2025-04-18 21:09:07 +0530

    Exception2.java

---
+++ Exception2.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+
+class InvalidInputException extends Exception {
+    InvalidInputException(String msg) {
+        super(msg);
+    }
+}
+
+public class Exception2 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("nonexistent.txt");
+            if (!file.exists()) {
+                throw new InvalidInputException("File does not exist!");
+            }
+            FileReader fr = new FileReader(file);
+        } catch (InvalidInputException | FileNotFoundException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
