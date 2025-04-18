425dcfef086c2edf018970b0db872c9320ffa50c
KrishnaSatpute23 <24satputek@rbunagpur.in>
2025-04-18 21:07:16 +0530

    Runnable.java

---
+++ Runnable.java
@@ -0,0 +1,13 @@
+
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Thread running: " + Thread.currentThread().getName());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyRunnable());
+        t.start();
+    }
+}
